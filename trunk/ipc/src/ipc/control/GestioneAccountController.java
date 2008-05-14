9fe3580e23d9d7d39ccd1b5881fbd2ab0b2c4c69
notargiacomo.s <notargiacomo.s@4929ac20-be49-0410-8c2c-950304d8a31f>
2008-05-14 10:21:03 +0000

    Attivazione account studente funzionante

--- trunk/ipc/src/ipc/control/GestioneAccountController.java
+++ trunk/ipc/src/ipc/control/GestioneAccountController.java
@@ -8,14 +8,15 @@ public class GestioneAccountController {
 
 	private List<Account> elencoAccountStudenti = null;
 	
-	public List<Account> getElencoAccountStudenti() {
+	public List<Account> getElencoAccountStudentiPendent() {
 		try {
 			SQLDAO sqlDao = new SQLDAO();
-			elencoAccountStudenti = sqlDao.listAccount();
-			for(int i=0; i<elencoAccountStudenti.size(); i++) {
-				if(!elencoAccountStudenti.get(i).getTipologia().equals("studente") ||
-						!elencoAccountStudenti.get(i).getStatus().equals("pendent"))
-					elencoAccountStudenti.remove(i);
+			List<Account> elenco = sqlDao.listAccount();
+			elencoAccountStudenti = new LinkedList<Account>();
+			for(int i=0; i<elenco.size(); i++) {
+				if(elenco.get(i).getTipologia().equals("studente") && elenco.get(i).getStatus().equals("pendent")) {
+					elencoAccountStudenti.add(elenco.get(i));
+				}
 			}
 			return elencoAccountStudenti;
 		} catch (Exception e) {
@@ -27,7 +28,7 @@ public class GestioneAccountController {
 	
 	public void abilitaAccountStudente(String email) {
 		Hashtable<String, String> data = new Hashtable<String, String>();
-		data.put("status", "abilitato");
+		data.put("status", "attivo");
 		try {
 			SQLDAO sqlDAO = new SQLDAO();
 			sqlDAO.updateAccount(email, data);

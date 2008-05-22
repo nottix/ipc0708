cf3a0f58369ee699cc60919665c87dd7c3a69e5b
notargiacomo.s <notargiacomo.s@4929ac20-be49-0410-8c2c-950304d8a31f>
2008-05-22 15:26:35 +0000

    fix

--- trunk/ipc/src/ipc/forms/CreazioneCorsoForm.java
+++ trunk/ipc/src/ipc/forms/CreazioneCorsoForm.java
@@ -128,11 +128,11 @@ public class CreazioneCorsoForm extends ActionForm {
 			errors.add("acronimo", new ActionError("acronimo.error"));
 		if((dataApertura != null) && (dataApertura.length() == 0))
 			errors.add("dataApertura", new ActionError("data.apertura.error"));
-		else if(!this.check_data(dataApertura))
+		else if(dataApertura!=null && !this.check_data(dataApertura))
 			errors.add("dataApertura", new ActionError("data.invalid.format"));
 		if((dataChiusura != null) && (dataChiusura.length() == 0))
 			errors.add("dataChiusura", new ActionError("data.chiusura.error"));
-		else if(!this.check_data(dataChiusura))
+		else if(dataChiusura!=null && !this.check_data(dataChiusura))
 			errors.add("dataChiusura", new ActionError("data.invalid.format"));
 //		if(dataApertura != null && dataChiusura != null)
 //			if(cmpDates(dataApertura, dataChiusura))
@@ -141,16 +141,16 @@ public class CreazioneCorsoForm extends ActionForm {
 	}
 	
 	 public boolean check_data(String data) {
-//			boolean test1 = Pattern.compile("[0-9]{1,2}/[0-9]{1,2}/20[0-9]{2}").matcher(data).matches();
-//			String []dates = data.split("/");
-//			if(Integer.valueOf(dates[0]) < 1 || Integer.valueOf(dates[0]) > 31)
-//				return false;
-//			if(Integer.valueOf(dates[1]) < 1 || Integer.valueOf(dates[1]) > 12)
-//				return false;
-//			if(Integer.valueOf(dates[2]) < 2000 || Integer.valueOf(dates[2]) > 2099)
-//				return false;
-//			return test1;
-		 return true;
+//		 boolean test1 = Pattern.compile("[0-9]{1,2}/[0-9]{1,2}/20[0-9]{2}").matcher(data).matches();
+		 boolean test1 = Pattern.compile("\\d\\d/\\d\\d/20\\d\\d").matcher(data).matches();
+		 String []dates = data.split("/");
+		 if(Integer.valueOf(dates[0]) < 1 || Integer.valueOf(dates[0]) > 31)
+			 return false;
+		 if(Integer.valueOf(dates[1]) < 1 || Integer.valueOf(dates[1]) > 12)
+			 return false;
+		 if(Integer.valueOf(dates[2]) < 2000 || Integer.valueOf(dates[2]) > 2099)
+			 return false;
+		 return test1;
 	 }
 	
 }
